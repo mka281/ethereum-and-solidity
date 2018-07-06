c810b747cfdb634bfcd38bc7edb9baf0a73f0363
mka <mka281@icloud.com>
2018-07-06 07:44:05 -0400

    Refactor to async/await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,15 +3,17 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
   // Get a list of all accounts
-  web3.eth.getAccounts().then(fetchAccounts => {
-    console.log(fetchAccounts);
-  });
+  accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
 });
 
 describe("Inbox", () => {
-  it("deploys a contract", () => {});
+  it("deploys a contract", () => {
+    console.log(accounts);
+  });
 });
